cd3968c40c254cb6398e24153d51599592765787
Vitor-Manoel02 <vitor@asupernova.com.br>
2023-10-05 14:11:14 -0300

    fix(hooks): use https with ngrok

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -14,7 +14,7 @@ export const useUserData = () => {
   const getUserById = async (id: string): Promise<User> => {
     console.log("🚀 ~ file: useUserData.ts ~ line 91 ~ getUserById ~ id", id);
 
-    const response = await axios.get(`https://f0f4-187-51-208-238.ngrok-free.app/users`, {
+    const response = await axios.get(`https://f989-187-51-208-238.ngrok-free.app//users`, {
       headers: {
         id,
       },
